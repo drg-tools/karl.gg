aae052674aeb159794f38e9e1e23f510c8f9d6ad
Chase Coney <chasiepoo@gmail.com>
2020-05-03 15:51:21 -0500

    style: create view mod selection matches rest of selections

--- resources/views/builds/create.blade.php
+++ resources/views/builds/create.blade.php
@@ -15,12 +15,28 @@
         {!! Form::textarea('description', null, ['class' => 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline']) !!}
     </div>
 
-    @livewire('character-selector', ['characters' => $characters])
+    <div id="character-selections">
+        @livewire('character-selector', ['characters' => $characters])
+    </div>
 
     <div class="mt-4">
     {!! Form::submit('Create', ['class' => 'bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-4 px-4 rounded focus:outline-none focus:shadow-outline']) !!}
     </div>
 
     {!! Form::close() !!}
+
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+    <script>
+        $('#character-selections').on('change', 'input:radio', function() {
+
+            let classes = "bg-blue-100 border-blue-400"
+
+            // Remove checked from all labels in row
+            $(this).closest('div').find('label').removeClass(classes);
+
+            // Apply checked to radio that is selected
+            $(this).closest('label').addClass(classes);
+        });
+    </script>
 @endsection
 
