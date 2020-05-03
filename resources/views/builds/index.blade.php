3b35fbe08556f4435c2140a64d1bc56e768e292d
Chase Coney <chasiepoo@gmail.com>
2020-05-02 23:04:57 -0500

    clean up index and show some mo

--- resources/views/builds/index.blade.php
+++ resources/views/builds/index.blade.php
@@ -19,6 +19,9 @@
                             Class
                         </th>
                         <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Guns
+                        </th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                             Last Updated
                         </th>
                         <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
@@ -43,11 +46,31 @@
                                     {{ $build->character->name }}
                                 </div>
                             </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">
+                                    @foreach($build->mods->groupBy('gun_id') as $mods)
+                                    <x-gun-icon :name="$mods[0]->gun->name" size="w-16" />
+                                    @endforeach
+                                </div>
+                            </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
                                 {{ $build->updated_at->diffForHumans() }}
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+{{--                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>--}}
+                                <div class="flex justify-evenly">
+                                    <div class="flex justify-evenly">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 text-gray-300 cursor-pointer mr-2" stroke="currentColor" fill="currentColor" viewBox="0 0 20 20">
+                                            <path d="M11.0010436,0 C9.89589787,0 9.00000024,0.886706352 9.0000002,1.99810135 L9,8 L1.9973917,8 C0.894262725,8 0,8.88772964 0,10 L0,12 L2.29663334,18.1243554 C2.68509206,19.1602453 3.90195042,20 5.00853025,20 L12.9914698,20 C14.1007504,20 15,19.1125667 15,18.000385 L15,10 L12,3 L12,0 L11.0010436,0 L11.0010436,0 Z M17,10 L20,10 L20,20 L17,20 L17,10 L17,10 Z" id="Fill-97"></path>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 text-gray-300 cursor-pointer" stroke="currentColor" fill="currentColor" viewBox="0 0 20 20">
+                                            <path d="M11.0010436,20 C9.89589787,20 9.00000024,19.1132936 9.0000002,18.0018986 L9,12 L1.9973917,12 C0.894262725,12 0,11.1122704 0,10 L0,8 L2.29663334,1.87564456 C2.68509206,0.839754676 3.90195042,8.52651283e-14 5.00853025,8.52651283e-14 L12.9914698,8.52651283e-14 C14.1007504,8.52651283e-14 15,0.88743329 15,1.99961498 L15,10 L12,17 L12,20 L11.0010436,20 L11.0010436,20 Z M17,10 L20,10 L20,0 L17,0 L17,10 L17,10 Z"></path>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 text-gray-300 cursor-pointer" stroke="currentColor" fill="currentColor" viewBox="0 0 20 20"><path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"></path></svg>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
