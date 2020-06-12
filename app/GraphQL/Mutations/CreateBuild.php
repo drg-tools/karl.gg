81e1a95845184e9d9cab8beec52eb149e885fe46
Chase Coney <chasiepoo@gmail.com>
2020-06-12 00:27:23 -0500

    feat: add rough mutations for builds

---
+++ app/GraphQL/Mutations/CreateBuild.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Build;
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Arr;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class CreateBuild
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param  mixed[]  $args The arguments that were passed into the field.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
+     */
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+	$build = Build::make($args);
+
+	// TODO: Remove when auth Passport implemented
+	$build->user_id = 1;
+	$build->save();
+
+	// TODO: do we assume mods is always here?
+	$build->mods()->sync(Arr::flatten($args['mods']));
+
+	return $build;
+    }
+}
