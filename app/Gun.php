f34fa4336e6cb65edf3115b9d85df9dbc6a6383d
Chase Coney <chasiepoo@gmail.com>
2020-06-26 22:52:36 -0500

    style: apply laravel style to all files

--- app/Gun.php
+++ app/Gun.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gun extends Model
 {
-
     public function mods()
     {
         return $this->hasMany(Mod::class);
--- database/factories/CharacterFactory.php
+++ database/factories/CharacterFactory.php
@@ -7,6 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Character::class, function (Faker $faker) {
     return [
-        'name' => 'Engineer'
+        'name' => 'Engineer',
     ];
 });
