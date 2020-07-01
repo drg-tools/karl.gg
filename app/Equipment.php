ce0b50617b6b5c2c77b252405fb5e0d6e36a475b
Andrew Nolan <andrew_nolan17@yahoo.com>
2020-07-01 11:06:42 -0400

    Styling fix

--- app/Equipment.php
+++ app/Equipment.php
@@ -6,7 +6,6 @@ use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Model;
 
-
 class Equipment extends Model
 {
     use Filterable, CrudTrait;
