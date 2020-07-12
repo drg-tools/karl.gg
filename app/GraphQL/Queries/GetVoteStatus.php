32c3b42b3b15a710be9cdfd7cf85aabd425b3a28
Chase Coney <chasiepoo@gmail.com>
2020-07-12 22:03:19 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/GraphQL/Queries/GetVoteStatus.php
+++ app/GraphQL/Queries/GetVoteStatus.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace App\GraphQL\Queries;
+
 use App\Loadout;
 use App\User;
-use Nagy\LaravelRating\Models\Rating;
-use Illuminate\Support\Facades\Auth;
 use GraphQL\Type\Definition\ResolveInfo;
-use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class GetVoteStatus
@@ -15,15 +14,15 @@ class GetVoteStatus
      * @param  null  $_
      * @param  array<string, mixed>  $args
      */
-     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
-     {
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
         $loadout = Loadout::findOrFail($args['id']);
         // TODO: handle guest user
         if (Auth::check()) {
-            $user  = User::find(auth()->user()->id);
+            $user = User::find(auth()->user()->id);
         }
-        $isUpVoted = $user->upVoted()->contains('id', $args['id'] );
+        $isUpVoted = $user->upVoted()->contains('id', $args['id']);
         // 0 no, 1 yes
         return $isUpVoted;
-     }
+    }
 }
