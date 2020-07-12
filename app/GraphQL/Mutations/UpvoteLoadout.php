d398afb1118aa2369bfc91b8014db0745b6509d7
Chase Coney <chasiepoo@gmail.com>
2020-07-12 17:15:08 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/GraphQL/Mutations/UpvoteLoadout.php
+++ app/GraphQL/Mutations/UpvoteLoadout.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace App\GraphQL\Mutations;
+
 use App\Loadout;
 use App\User;
-use Nagy\LaravelRating\Models\Rating;
-use Illuminate\Support\Facades\Auth;
 use GraphQL\Type\Definition\ResolveInfo;
-use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class UpvoteLoadout
@@ -15,19 +14,20 @@ class UpvoteLoadout
      * @param  null  $_
      * @param  array<string, mixed>  $args
      */
-     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
         $loadout = Loadout::findOrFail($args['id']);
         // TODO: handle guest user
         if (Auth::check()) {
-            $user  = User::find(auth()->user()->id);
+            $user = User::find(auth()->user()->id);
         }
-        $isUpVoted = $user->upVoted()->contains('id', $args['id'] );
+        $isUpVoted = $user->upVoted()->contains('id', $args['id']);
         if ($isUpVoted) {
             $user->downvote($loadout);
         } else {
             $user->upVote($loadout);
         }
+
         return $loadout;
     }
 }
