c997b0e444175b6e8d5bd0cb98ecf3b295a67587
surmiran <zoid@gmx.ch>
2020-07-25 23:20:21 +0200

    Example for svg clickable areas on hero image.

--- resources/js/components/CallToAction.vue
+++ resources/js/components/CallToAction.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="callToActionContainer">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 900 250" preserveAspectRatio="xMinYMin meet">
+            <image width="900" height="250" xlink:href="../assets/img/karl-hero-wide.png">
+            </image>
+            <a xlink:href="/build/D" class="hoverEffect">
+                <rect x="500" y="100" fill="#fff" fill-opacity="0" stroke="#000" stroke-opacity="0"  stroke-width="5" stroke-linecap="butt" width="70" height="150" ></rect>
+            </a>
+        </svg>
+    </div>
+    <!--<div class="callToActionContainer">
         <img class="wideHero" src="../assets/img/karl-hero-wide.png" alt="hero image" usemap="#wideHeroMap" width="100%" height="250">
         <img class="highHero" src="../assets/img/karl-hero-high.png" alt="hero image" usemap="#wideHeroMap" width="100%" height="250">
-        <!-- todo: use svg for clickable areas. http://thenewcode.com/760/Create-A-Responsive-Imagemap-With-SVG -->
+        &lt;!&ndash; todo: use svg for clickable areas. http://thenewcode.com/760/Create-A-Responsive-Imagemap-With-SVG &ndash;&gt;
         <map name="wideHeroMap">
             <area shape="rect" coords="500, 100, 570, 250" alt="random" href="/build/D">
             <area shape="rect" coords="580, 100, 640, 250" alt="random" href="/build/E">
@@ -10,7 +19,7 @@
             <area shape="rect" coords="740, 100, 795, 250" alt="random" href="/build/S">
             <area shape="rect" coords="400, 80, 450, 110" alt="random" href="/build/R">
         </map>
-    </div>
+    </div>-->
 </template>
 
 <script>
@@ -48,5 +57,9 @@
         .highHero {
             display: inherit;
         }
+    }
+
+    .hoverEffect:hover > rect {
+        stroke-opacity: 1;
     }
 </style>
