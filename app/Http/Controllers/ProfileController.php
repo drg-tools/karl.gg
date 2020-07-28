43225b61904d9ede8241568a1e9aed641e46b911
Andrew Nolan <andrew_nolan17@yahoo.com>
2020-07-27 22:11:36 -0400

    Edit profile scaffolding

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -23,6 +23,28 @@ class ProfileController extends Controller
         return view('profile', ['user' => User::findOrFail($id), 'loadouts' => $loadoutsWithSalutes, 'loadoutCount' => $loadoutCount, 'salutesCount' => $salutesCount]);
     }
 
+    /**
+     * Allow the current user to edit their current profile
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function editProfile($id)
+    {
+        return view('editprofile', ['user' => User::findOrFail($id)]);
+    }
+    /**
+     * Update the user's profile.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function editProfileSave(Request $request)
+    {
+        // $request->user() returns an instance of the authenticated user...
+    }
+    
+
     private function getLoadoutsForUser($userId) {
         $user = User::findOrFail($userId);
         $loadouts = $user->loadouts->all();
