60dbf7ee2a3174f575d8d71f391602e4829e96ba
Andrew Nolan <andrew_nolan17@yahoo.com>
2020-08-10 14:16:43 -0400

    can update mod icon now

--- app/Mod.php
+++ app/Mod.php
@@ -11,15 +11,17 @@ class Mod extends Model
     use Filterable, CrudTrait;
 
     public $identifiableAttribute = 'name';
+    protected $guarded = ['id'];
+    public $timestamps = false;
 
     public function gun()
     {
-        return $this->belongsTo(Gun::class);
+        return $this->belongsTo(Gun::class, 'gun_id');
     }
 
     public function character()
     {
-        return $this->belongsTo(Character::class);
+        return $this->belongsTo(Character::class, 'character_id');
     }
 
     public function loadouts()
