46e49f74e6fd7a01f041f19869866d823d67169d
Andrew Nolan <andrew_nolan17@yahoo.com>
2020-08-11 19:25:27 -0400

    fix: backpack config

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -105,7 +105,7 @@ return [
     // change background color with bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'DRG Builds',
+    'developer_name' => 'Karl: Born Ready',
 
     // Developer website. Link in footer. Type false if you want to hide it.
     'developer_link' => 'https://karl.gg',
