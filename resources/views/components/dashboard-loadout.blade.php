cfc164ee580ea031ef24a60f3e8c5c8fcd318e55
Chase Coney <chasiepoo@gmail.com>
2020-08-21 10:58:16 -0500

    fix(dashboard): limit title length

--- resources/views/components/dashboard-loadout.blade.php
+++ resources/views/components/dashboard-loadout.blade.php
@@ -4,7 +4,7 @@
 	    <div class="titleRow">
 		<div class="titleContentLeft">
 		    <img src="/assets/img/{{ $loadout->character->image }}.png" class="classIcon"/>
-		    <h2 class="buildName">{{ $loadout->name }}</h2>
+		    <h2 class="buildName">{{ Str::limit($loadout->name, 30) }}</h2>
                 </div>
 		<div class="titleContentRight">
 		    <div class="weaponContainer">
