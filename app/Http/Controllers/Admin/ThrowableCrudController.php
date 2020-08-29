5608862b0ef856b51087eacdd62e759241d963d3
Andrew Nolan <andrew_nolan17@yahoo.com>
2020-08-29 13:54:50 -0400

    Throwables done

--- app/Http/Controllers/Admin/ThrowableCrudController.php
+++ app/Http/Controllers/Admin/ThrowableCrudController.php
@@ -26,16 +26,118 @@ class ThrowableCrudController extends CrudController
 
     protected function setupListOperation()
     {
-        // TODO: remove setFromDb() and manually define Columns, maybe Filters
-        $this->crud->setFromDb();
+        $this->crud->addColumns(['name']); // make these the only columns in the table
+        $this->crud->addColumn([
+            'name' => 'character', // The db column name
+            'label' => 'Character', // Table column heading
+            'type' => 'relationship',
+            'entity' => 'character', // the method that defines the relationship in your Model
+            'attribute' => 'name', // foreign key attribute that is shown to user
+            'model' => App\Character::class, // foreign key model
+            'searchLogic' => function ($query, $column, $searchTerm) {
+                $query->orWhereHas('character', function ($q) use ($searchTerm) {
+                    $q->where('name', 'like', '%'.$searchTerm.'%');
+                });
+            },
+        ]);
+        $this->crud->addFilter([
+            'type' => 'dropdown',
+            'name' => 'class',
+            'label' => 'Class',
+        ], [
+            1 => 'Engineer',
+            2 => 'Scout',
+            3 => 'Driller',
+            4 => 'Gunner',
+        ], function ($value) { // if the filter is active
+            $this->crud->addClause('where', 'character_id', $value);
+        });
     }
 
     protected function setupCreateOperation()
     {
         $this->crud->setValidation(ThrowableRequest::class);
 
-        // TODO: remove setFromDb() and manually define Fields
-        $this->crud->setFromDb();
+        $this->crud->addField([
+            'name' => 'name',
+            'label' => 'Throwable Name',
+            'type' => 'text',
+            'tab' => 'Base Info',
+        ]);
+        $this->crud->addField([
+            'type' => 'select',
+            'name' => 'character_id', // the relationship name in your Model
+            'entity' => 'character', // the relationship name in your Model
+            'attribute' => 'name', // attribute on Article that is shown to admin
+            'model' => "App\Character",
+            'tab' => 'Base Info',
+        ]);
+        $this->crud->addField([
+            'name' => 'description',
+            'label' => 'Text Description',
+            'type' => 'text',
+            'tab' => 'Base Info',
+        ]);
+        $this->crud->addField([
+            'name' => 'icon',
+            'label' => 'Icon',
+            'type' => 'text',
+            'tab' => 'Base Info',
+        ]);
+        $this->crud->addField([
+            'name' => 'patch_id',
+            'label' => 'Patch Number Index',
+            'type' => 'text',
+            'tab' => 'Base Info',
+        ]);
+        $this->crud->addField([
+            'name' => 'credits_cost',
+            'label' => 'Credits Cost',
+            'type' => 'number',
+            'tab' => 'Cost',
+        ]);
+        $this->crud->addField([
+            'name' => 'magnite_cost',
+            'label' => 'Magnite Cost',
+            'type' => 'number',
+            'tab' => 'Cost',
+        ]);
+        $this->crud->addField([
+            'name' => 'bismor_cost',
+            'label' => 'Bismor Cost',
+            'type' => 'number',
+            'tab' => 'Cost',
+        ]);
+        $this->crud->addField([
+            'name' => 'umanite_cost',
+            'label' => 'Umanite Cost',
+            'type' => 'number',
+            'tab' => 'Cost',
+        ]);
+        $this->crud->addField([
+            'name' => 'croppa_cost',
+            'label' => 'Croppa Cost',
+            'type' => 'number',
+            'tab' => 'Cost',
+        ]);
+        $this->crud->addField([
+            'name' => 'enor_pearl_cost',
+            'label' => 'Enor Pearl Cost',
+            'type' => 'number',
+            'tab' => 'Cost',
+        ]);
+        $this->crud->addField([
+            'name' => 'jadiz_cost',
+            'label' => 'Jadiz Cost',
+            'type' => 'number',
+            'tab' => 'Cost',
+        ]);
+        $this->crud->addField([
+            'name' => 'json_stats',
+            'label' => 'JSON Stats',
+            'type' => 'textarea',
+            'tab' => 'Stats',
+        ]);
     }
 
     protected function setupUpdateOperation()
