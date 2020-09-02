488f3bf834bf97a954a7a6eabc3a570210341f50
Chase Coney <chasiepoo@gmail.com>
2020-09-01 21:08:43 -0500

    chore: rollback last change, i don't even know anymore...

--- config/backup.php
+++ config/backup.php
@@ -95,7 +95,7 @@ return [
             /*
              * The filename prefix used for the backup zip file.
              */
-            'filename_prefix' => 'backups',
+            'filename_prefix' => '',
 
             /*
              * The disk names on which the backups will be stored.
