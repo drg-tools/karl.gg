7686f3553f5f4747968654bc39787486706492be
Chase Coney <chasiepoo@gmail.com>
2020-09-08 12:39:00 -0500

    chore: monitor s3 backups

--- config/backup.php
+++ config/backup.php
@@ -168,7 +168,7 @@ return [
     'monitor_backups' => [
         [
             'name' => env('APP_NAME', 'laravel-backup'),
-            'disks' => ['local'],
+            'disks' => ['s3'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
