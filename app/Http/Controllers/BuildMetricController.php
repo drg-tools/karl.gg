ca961b47e1ea367fa2649864aff52288d258cee8
Chase Coney <chaseconey@users.noreply.github.com>
2020-11-27 20:48:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/BuildMetricController.php
+++ app/Http/Controllers/BuildMetricController.php
@@ -2,30 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\BuildMetric;
 use App\Character;
 use App\Gun;
-use App\Loadout;
-use App\BuildMetric;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Http\Request;
-use Str;
 
 class BuildMetricController extends Controller
 {
-    public function index(Request $request,$class,$gun,$combo)
+    public function index(Request $request, $class, $gun, $combo)
     {
         // SEOTools::setTitle('Deep Rock Galactic Loadouts');
 
         $build = BuildMetric::where([
-            ['character_id','=', $class],
-            ['gun_id','=', $gun],
-            ['build_combination','=', $combo]
+            ['character_id', '=', $class],
+            ['gun_id', '=', $gun],
+            ['build_combination', '=', $combo],
         ])->get();
 
-        $build_gun = Gun::where('id',$gun)->get();
-        $build_character = Character::where('id',$class)->get();
-
-
+        $build_gun = Gun::where('id', $gun)->get();
+        $build_character = Character::where('id', $class)->get();
 
         return view('asv.index', [
             'build'     => $build,
