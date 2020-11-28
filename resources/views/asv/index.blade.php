d456e900d5faa6bf684a5b9d68832a3b53bf53ac
Andrew Nolan <andrew_nolan17@yahoo.com>
2020-11-28 13:42:33 -0500

    feat: poc mod matrix images, no tooltip

--- resources/views/asv/index.blade.php
+++ resources/views/asv/index.blade.php
@@ -4,24 +4,38 @@
 
     <div class="featuredLoadoutsContainer">
         {{-- {{var_dump($build)}} --}}
-        @foreach ($modMatrix['gun_mods'] as $key => $mod_tier)
-        <p>
-            {{$key}}
-            @foreach ($mod_tier as $mod)
-                @if($modMatrix['selected_index'][$key]['selected'])
-                    @if ($modMatrix['selected_index'][$key]['value'] == $mod->mod_index)
-                        TRUE
-                        @else
-                        FALSE
+        <div class="modMatrixContainer">
+            
+            @foreach ($modMatrix['gun_mods'] as $key => $mod_tier)
+            <div class="modMatrixRow">
+                @foreach ($mod_tier as $mod)
+                <div class="mod">
+                        @if ($modMatrix['selected_index'][$key]['value'] == $mod->mod_index)
+                                <svg viewBox="0 0 80 50"
+                                        height="100%"
+                                        role="img"
+                                        class="modActive"
+                                >
+                                    <path
+                                        d=" M 0.3679663,25 13.7826,0.609756 H 66.221625 L 79.636259,25 66.221625,49.390244 H 13.7826L 0.3679663,25"/>
+                                </svg>
+                    @else
+                    <svg viewBox="0 0 80 50"
+                                        height="100%"
+                                        role="img"
+                                        class="modInactive"
+                                >
+                                    {{-- <title>{{ mod.mod_name }}</title> --}}
+                                    {{-- <desc>{{ mod.text_description }}</desc> --}}
+                                    <path
+                                        d=" M 0.3679663,25 13.7826,0.609756 H 66.221625 L 79.636259,25 66.221625,49.390244 H 13.7826L 0.3679663,25"/>
+                                </svg>
                     @endif
-                @else
-                FALSE
-                @endif
+                            </div><!-- end mod  -->
+                @endforeach
+                </div><!-- end mod matrix row -->
             @endforeach
-        </p>
-        @endforeach
-        
-         </equipment-card> 
+        </div><!-- end mod matrix container -->
     </div>
 
 @endsection
