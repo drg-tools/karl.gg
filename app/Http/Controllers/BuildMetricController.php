8d16b2617bfbbcc47a604954922565c3377245c4
Chase Coney <chaseconey@users.noreply.github.com>
2020-11-28 23:27:50 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/BuildMetricController.php
+++ app/Http/Controllers/BuildMetricController.php
@@ -2,39 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\BuildMetric;
 use App\Character;
 use App\Gun;
-use App\Mod;
-use App\Loadout;
 use App\Overclock;
-use App\BuildMetric;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Http\Request;
-use Str;
 
 class BuildMetricController extends Controller
 {
-    public function index(Request $request,$class,$gun,$combo)
+    public function index(Request $request, $class, $gun, $combo)
     {
         // SEOTools::setTitle('Deep Rock Galactic Loadouts');
         // TODO: SEO
 
         $build = BuildMetric::where([
-            ['character_id','=', $class],
-            ['gun_id','=', $gun],
-            ['build_combination','=', $combo]
+            ['character_id', '=', $class],
+            ['gun_id', '=', $gun],
+            ['build_combination', '=', $combo],
         ])->get();
 
-        $build_gun = Gun::where('id',$gun)->get();
+        $build_gun = Gun::where('id', $gun)->get();
         $gun_icon = asset('/assets/'.$build_gun[0]->image.'.svg');
-        $build_character = Character::where('id',$class)->get();
+        $build_character = Character::where('id', $class)->get();
 
-        $mod_matrix = $this->getModMatrix($gun,$combo);
+        $mod_matrix = $this->getModMatrix($gun, $combo);
         $combo_array = str_split($combo);
-        if($combo_array[5] != "-") {
-            $overclock = $this->getOverclock($gun,$combo_array[5]);
+        if ($combo_array[5] != '-') {
+            $overclock = $this->getOverclock($gun, $combo_array[5]);
         }
-        
+
         return view('asv.index', [
             'buildMetrics'  => $build,
             'gun'           => $build_gun,
@@ -46,23 +43,23 @@ class BuildMetricController extends Controller
         ]);
     }
 
-    private function getModMatrix($gun,$combo) 
+    private function getModMatrix($gun, $combo)
     {
         $selected_index = [];
         $gun_object = Gun::find($gun);
         $gun_mods = $gun_object->mods->groupBy('mod_tier');
-        
+
         $combo_array = str_split($combo);
-        
-        foreach($combo_array as $key => $tier) {
+
+        foreach ($combo_array as $key => $tier) {
             $selected = false;
-            if($tier != '-') {
+            if ($tier != '-') {
                 $selected = true;
             }
             // dd($tier);
-            $selected_index[$key+1] = array('value' => $tier, 'selected' => $selected );
+            $selected_index[$key + 1] = ['value' => $tier, 'selected' => $selected];
         }
-        $matrixArray = compact('gun_mods','selected_index');
+        $matrixArray = compact('gun_mods', 'selected_index');
 
         return $matrixArray;
     }
@@ -71,9 +68,9 @@ class BuildMetricController extends Controller
     {
         $gun_object = Gun::find($gun);
         $overclock = Overclock::where([
-            ['character_id','=', $gun_object->character_id],
-            ['gun_id','=', $gun_object->id],
-            ['overclock_index','=', $index]
+            ['character_id', '=', $gun_object->character_id],
+            ['gun_id', '=', $gun_object->id],
+            ['overclock_index', '=', $index],
         ])->get();
 
         return $overclock;
--- database/seeds/BuildMetricSeeder.php
+++ database/seeds/BuildMetricSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
 // composer require laracasts/testdummy
 use Laracasts\TestDummy\Factory as TestDummy;
 
@@ -13,8 +12,8 @@ class BuildMetricSeederTableSeeder extends Seeder
     public function run()
     {
         $row = 1;
-        if (($handle = fopen(base_path("database/seeds/build_metrics_import.csv"), "r")) !== false) {
-            while (($data = fgetcsv($handle, 0, ";")) !== false) {
+        if (($handle = fopen(base_path('database/seeds/build_metrics_import.csv'), 'r')) !== false) {
+            while (($data = fgetcsv($handle, 0, ';')) !== false) {
                 if ($row === 1) {
                     $row++;
                     continue;
@@ -22,53 +21,51 @@ class BuildMetricSeederTableSeeder extends Seeder
                 $row++;
 
                 $dbData = [
-                    'id'                               => '"'.$data[0].'"', 
-                    'character_id'                     => '"'.$data[1].'"', 
-                    'gun_id'                           => '"'.$data[2].'"', 
-                    'weapon_short_name'                => '"'.$data[3].'"', 
-                    'build_combination'                => '"'.$data[4].'"', 
-                    'ideal_burst_dps'                  => '"'.$data[5].'"', 
-                    'burst_dps_wp'                     => '"'.$data[6].'"', 
-                    'burst_dps_acc'                    => '"'.$data[7].'"', 
-                    'burst_dps_aw'                     => '"'.$data[8].'"', 
-                    'burst_dps_wp_acc'                 => '"'.$data[9].'"', 
-                    'burst_dps_wp_aw'                  => '"'.$data[10].'"', 
-                    'burst_dps_acc_aw'                 => '"'.$data[11].'"', 
-                    'burst_dps_wp_acc_aw'              => '"'.$data[12].'"', 
-                    'ideal_sustained_dps'              => '"'.$data[13].'"', 
-                    'sustained_dps_wp'                 => '"'.$data[14].'"', 
-                    'sustained_dps_acc'                => '"'.$data[15].'"', 
-                    'sustained_dps_aw'                 => '"'.$data[16].'"', 
-                    'sustained_dps_wp_acc'             => '"'.$data[17].'"', 
-                    'sustained_dps_wp_aw'              => '"'.$data[18].'"', 
-                    'sustained_dps_acc_aw'             => '"'.$data[19].'"', 
-                    'sustained_dps_wp_acc_aw'          => '"'.$data[20].'"', 
-                    'ideal_additional_target_dps'      => '"'.$data[21].'"', 
-                    'max_num_targets_per_shot'         => '"'.$data[22].'"', 
-                    'max_multi_target_damage'          => '"'.$data[23].'"', 
-                    'ammo_efficiency'                  => '"'.$data[24].'"', 
-                    'damage_wasted_by_armor'           => '"'.$data[25].'"', 
-                    'general_accuracy'                 => '"'.$data[26].'"', 
-                    'weakpoint_accuracy'               => '"'.$data[27].'"', 
-                    'firing_duration'                  => '"'.$data[28].'"', 
-                    'average_time_to_kill'             => '"'.$data[29].'"', 
-                    'average_overkill'                 => '"'.$data[30].'"', 
-                    'breakpoints'                      => '"'.$data[31].'"', 
-                    'utility'                          => '"'.$data[32].'"', 
-                    'average_time_to_ignite_or_freeze' => '"'.$data[33].'"', 
-                    'damage_per_magazine'              => '"'.$data[34].'"', 
-                    'time_to_fire_magazine'            => '"'.$data[35].'"', 
-                    'patch_id'                         => '"'.$data[36].'"', 
+                    'id'                               => '"'.$data[0].'"',
+                    'character_id'                     => '"'.$data[1].'"',
+                    'gun_id'                           => '"'.$data[2].'"',
+                    'weapon_short_name'                => '"'.$data[3].'"',
+                    'build_combination'                => '"'.$data[4].'"',
+                    'ideal_burst_dps'                  => '"'.$data[5].'"',
+                    'burst_dps_wp'                     => '"'.$data[6].'"',
+                    'burst_dps_acc'                    => '"'.$data[7].'"',
+                    'burst_dps_aw'                     => '"'.$data[8].'"',
+                    'burst_dps_wp_acc'                 => '"'.$data[9].'"',
+                    'burst_dps_wp_aw'                  => '"'.$data[10].'"',
+                    'burst_dps_acc_aw'                 => '"'.$data[11].'"',
+                    'burst_dps_wp_acc_aw'              => '"'.$data[12].'"',
+                    'ideal_sustained_dps'              => '"'.$data[13].'"',
+                    'sustained_dps_wp'                 => '"'.$data[14].'"',
+                    'sustained_dps_acc'                => '"'.$data[15].'"',
+                    'sustained_dps_aw'                 => '"'.$data[16].'"',
+                    'sustained_dps_wp_acc'             => '"'.$data[17].'"',
+                    'sustained_dps_wp_aw'              => '"'.$data[18].'"',
+                    'sustained_dps_acc_aw'             => '"'.$data[19].'"',
+                    'sustained_dps_wp_acc_aw'          => '"'.$data[20].'"',
+                    'ideal_additional_target_dps'      => '"'.$data[21].'"',
+                    'max_num_targets_per_shot'         => '"'.$data[22].'"',
+                    'max_multi_target_damage'          => '"'.$data[23].'"',
+                    'ammo_efficiency'                  => '"'.$data[24].'"',
+                    'damage_wasted_by_armor'           => '"'.$data[25].'"',
+                    'general_accuracy'                 => '"'.$data[26].'"',
+                    'weakpoint_accuracy'               => '"'.$data[27].'"',
+                    'firing_duration'                  => '"'.$data[28].'"',
+                    'average_time_to_kill'             => '"'.$data[29].'"',
+                    'average_overkill'                 => '"'.$data[30].'"',
+                    'breakpoints'                      => '"'.$data[31].'"',
+                    'utility'                          => '"'.$data[32].'"',
+                    'average_time_to_ignite_or_freeze' => '"'.$data[33].'"',
+                    'damage_per_magazine'              => '"'.$data[34].'"',
+                    'time_to_fire_magazine'            => '"'.$data[35].'"',
+                    'patch_id'                         => '"'.$data[36].'"',
                 ];
-
-                
 
                 $colNames = array_keys($dbData);
 
                 $createQuery = 'INSERT INTO build_metrics ('.implode(',', $colNames).') VALUES ('.implode(',', $dbData).')';
 
                 DB::statement($createQuery, $data);
-                $this->command->info('Build Metrics Row: ' . $row);
+                $this->command->info('Build Metrics Row: '.$row);
             }
             fclose($handle);
         }
