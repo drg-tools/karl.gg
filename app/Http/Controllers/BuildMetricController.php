720ddd7dabfa501506bad60ce768416288f62ce6
Chase Coney <chaseconey@users.noreply.github.com>
2020-11-28 20:28:46 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/BuildMetricController.php
+++ app/Http/Controllers/BuildMetricController.php
@@ -2,38 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\BuildMetric;
 use App\Character;
 use App\Gun;
 use App\Mod;
-use App\Loadout;
-use App\BuildMetric;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Http\Request;
-use Str;
 
 class BuildMetricController extends Controller
 {
-    public function index(Request $request,$class,$gun,$combo)
+    public function index(Request $request, $class, $gun, $combo)
     {
         // SEOTools::setTitle('Deep Rock Galactic Loadouts');
 
         //TODO: Equipment handling
 
         $build = BuildMetric::where([
-            ['character_id','=', $class],
-            ['gun_id','=', $gun],
-            ['build_combination','=', $combo]
+            ['character_id', '=', $class],
+            ['gun_id', '=', $gun],
+            ['build_combination', '=', $combo],
         ])->get();
 
-        $build_gun = Gun::where('id',$gun)->get();
+        $build_gun = Gun::where('id', $gun)->get();
         $gun_icon = asset('/assets/'.$build_gun[0]->image.'.svg');
         // dd($gun_icon);
-         $build_character = Character::where('id',$class)->get();
+        $build_character = Character::where('id', $class)->get();
 
         // TODO: Get Mod Matrix
         // TODO: Get Overclock based on combo
-        $mod_matrix = $this->getModMatrix($gun,$combo);
-        
+        $mod_matrix = $this->getModMatrix($gun, $combo);
+
         return view('asv.index', [
             'build'     => $build,
             'gun'       => $build_gun,
@@ -43,23 +41,23 @@ class BuildMetricController extends Controller
         ]);
     }
 
-    private function getModMatrix($gun,$combo) 
+    private function getModMatrix($gun, $combo)
     {
         $selected_index = [];
         $gun_object = Gun::find($gun);
         $gun_mods = $gun_object->mods->groupBy('mod_tier');
-        
+
         $combo_array = str_split($combo);
-        
-        foreach($combo_array as $key => $tier) {
+
+        foreach ($combo_array as $key => $tier) {
             $selected = false;
-            if($tier != '-') {
+            if ($tier != '-') {
                 $selected = true;
             }
             // dd($tier);
-            $selected_index[$key+1] = array('value' => $tier, 'selected' => $selected );
+            $selected_index[$key + 1] = ['value' => $tier, 'selected' => $selected];
         }
-        $matrixArray = compact('gun_mods','selected_index');
+        $matrixArray = compact('gun_mods', 'selected_index');
 
         return $matrixArray;
     }
