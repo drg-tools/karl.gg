4c8e7490473142524b089289c647ae645fbdc3b3
Andrew Nolan <andrew_nolan17@yahoo.com>
2020-12-07 13:23:55 -0500

    fix: scoping of build metric functions

--- app/BuildMetric.php
+++ app/BuildMetric.php
@@ -24,4 +24,38 @@ class BuildMetric extends Model
     {
         return $this->belongsTo(Patch::class);
     }
+
+    public function getModMatrix($gun, $combo)
+    {
+        $selected_index = [];
+        $gun_object = $gun;
+        $gun_mods = $gun_object->mods->groupBy('mod_tier');
+
+        $combo_array = str_split($combo);
+
+        foreach ($combo_array as $key => $tier) {
+            $selected = false;
+            if ($tier != '-') {
+                $selected = true;
+            }
+            // dd($tier);
+            $selected_index[$key + 1] = ['value' => $tier, 'selected' => $selected];
+        }
+        $matrixArray = compact('gun_mods', 'selected_index');
+
+        return $matrixArray;
+    }
+
+    public function getOverclock($gun, $index)
+    {
+        $gun_object = Gun::findOrFail($gun);
+        $overclock = Overclock::where([
+            ['character_id', '=', $gun_object->character_id],
+            ['gun_id', '=', $gun_object->id],
+            ['overclock_index', '=', $index],
+        ])->firstOrFail();
+
+        return $overclock;
+    }
+
 }
--- app/Http/Controllers/BuildMetricController.php
+++ app/Http/Controllers/BuildMetricController.php
@@ -23,11 +23,11 @@ class BuildMetricController extends Controller
 
         $build_gun = $build->gun;
 
-        $mod_matrix = $this->getModMatrix($build_gun, $combo);
+        $mod_matrix = $build->getModMatrix($build_gun, $combo);
         $combo_array = str_split($combo);
         $overclock = false;
         if ($combo_array[5] != '-') {
-            $overclock = $this->getOverclock($gun, $combo_array[5]);
+            $overclock = $build->getOverclock($gun, $combo_array[5]);
         }
 
         return view('asv.index', [
@@ -39,36 +39,4 @@ class BuildMetricController extends Controller
         ]);
     }
 
-    private function getModMatrix($gun, $combo)
-    {
-        $selected_index = [];
-        $gun_object = $gun;
-        $gun_mods = $gun_object->mods->groupBy('mod_tier');
-
-        $combo_array = str_split($combo);
-
-        foreach ($combo_array as $key => $tier) {
-            $selected = false;
-            if ($tier != '-') {
-                $selected = true;
-            }
-            // dd($tier);
-            $selected_index[$key + 1] = ['value' => $tier, 'selected' => $selected];
-        }
-        $matrixArray = compact('gun_mods', 'selected_index');
-
-        return $matrixArray;
-    }
-
-    private function getOverclock($gun, $index)
-    {
-        $gun_object = Gun::findOrFail($gun);
-        $overclock = Overclock::where([
-            ['character_id', '=', $gun_object->character_id],
-            ['gun_id', '=', $gun_object->id],
-            ['overclock_index', '=', $index],
-        ])->firstOrFail();
-
-        return $overclock;
-    }
 }
