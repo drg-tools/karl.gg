e65de1c07978d5a19ca5db999f1b0deaa5c0039b
Chase Coney <chasiepoo@gmail.com>
2021-03-06 18:31:43 -0600

    feat: open up api support for the chosen

--- resources/views/settings/tokens.blade.php
+++ resources/views/settings/tokens.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+@endsection
+
 @section('content')
     <!-- todo: style this page without bootstrap css.. -->
     <passport-personal-access-tokens></passport-personal-access-tokens>
+
+    <passport-authorized-clients></passport-authorized-clients>
+
+    <passport-clients></passport-clients>
+@endsection
+
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 @endsection
