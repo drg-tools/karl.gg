bfc654af767e115f1d67bb92761f6f3d351817de
Chase Coney <chasiepoo@gmail.com>
2021-05-02 17:24:08 -0500

    feat(blog): first pass redesign of post show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,10 +1,13 @@
-@extends('layouts.app')
+@extends('layouts.app-v2')
+
+@section('header')
+    {{ $post->title }}
+@endsection
 
 @section('content')
-    <div class="p-4 content-container">
-        <div class="text-sm underline"><a href="{{ route('blog.index') }}">Back</a></div>
-        <h2 class="text-orange mb-0">{{ $post->title }}</h2>
-        <div class="mb-4 text-white">{{ $post->published_at->diffForHumans() }}</div>
-        <article class="prose prose-xl max-w-full text-white">{!! $post->content !!}</article>
+    <div class="text-sm underline text-gray-300 my-2 ml-2 sm:ml-0"><a href="{{ route('blog.index') }}">Back</a></div>
+    <div class="p-4 text-gray-300 bg-gray-700 sm:rounded">
+        <div class="mb-4 text-gray-500">{{ $post->published_at->diffForHumans() }}</div>
+        <article class="prose lg:prose-xl max-w-full text-gray-300">{!! $post->content !!}</article>
     </div>
 @endsection
