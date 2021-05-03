d1e658b8af5f1fe6808d70d6cd634fc78f160340
Chase Coney <chasiepoo@gmail.com>
2021-05-02 19:38:05 -0500

    feat(asv): first pass redesign

--- resources/views/asv/show.blade.php
+++ resources/views/asv/show.blade.php
@@ -1,13 +1,117 @@
-@extends('layouts.app')
+@extends('layouts.app-v2')
+
+@section('header')
+    {{ $gun['name'] }}
+@endsection
+
+@section('styles')
+    <style>
+        /**
+* Equipment Card Component
+* */
+        .asvEquipmentCardContainer {
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
+            width: 10rem;
+        }
+        .asvModMatrixContainer {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 1rem;
+        }
+
+        .asvModMatrixRow {
+            display: flex;
+            height: 1rem;
+            width: 6rem;
+            margin-bottom: 0.3rem;
+        }
+
+        .asvMod {
+            width: 2rem;
+            height: 100%;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .asvModActive {
+            stroke: #fc9e00;
+            fill: #fc9e00;
+            stroke-width: 5px;
+        }
+
+        .asvModInactive {
+            stroke: #fc9e00;
+            fill: transparent;
+            stroke-width: 5px;
+        }
+
+        .flexboxWeaponSelect {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 4rem;
+        }
+
+        .asvIcon {
+            fill: #ada195;
+            width: 180px;
+        }
+
+        .flexboxWeaponSelect > svg {
+            margin: 0.5rem;
+        }
+
+        .overclockContainer {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .overclockName {
+            margin-left: 0.4rem;
+        }
+
+        .baseButton {
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0.5rem 0 0.5rem 1rem;
+            min-width: 8rem;
+            height: 2.2rem;
+            background: linear-gradient(90deg, #fc9e00 4%, rgba(0, 0, 0, 0) 4%, rgba(0, 0, 0, 0) 8%, #fc9e00 8%, #fc9e00 92%, rgba(0, 0, 0, 0) 92%, rgba(0, 0, 0, 0) 96%, #fc9e00 96%);
+        }
+        /* todo: base all hover effects on this one, like buttons ingame */
+        .baseButton:hover {
+            background: linear-gradient(90deg, #ffd200 4%, rgba(0, 0, 0, 0) 4%, rgba(0, 0, 0, 0) 8%, #ffd200 8%, #ffd200 92%, rgba(0, 0, 0, 0) 92%, rgba(0, 0, 0, 0) 96%, #ffd200 96%);
+        }
+
+        .baseButtonText {
+            color: #000000;
+        // font-size: 1.6rem;
+            letter-spacing: 0.05rem;
+            font-weight: bold;
+            font-family: BebasNeue, sans-serif;
+        }
+
+    </style>
+@endsection
 
 @section('content')
 
-    <div class="content-container pb-20 text-white">
+
+    <div class="my-2">
+        <a href="{{ url()->previous() }}" class="underline text-gray-300 ml-2 sm:ml-0">Back</a>
+    </div>
+
+    <div class="bg-gray-700 pb-20 text-white sm:rounded">
         @include('components.asv-alert')
         <div class="flex flex-col md:flex-row justify-between">
-            <div class="mx-4 pt-2">
-                <h1 class="mr-4">{{ $gun['name'] }}</h1>
-                <h3 class="text-off-white mr-4">
+            <div class="mx-4 pt-2 text-gray-300 text-xl">
+                <h3 class="mr-4">
                     @if($gun['character_slot'] == 1)
                         Primary Weapon
                     @elseif($gun['character_slot'] == 2)
@@ -16,17 +120,12 @@
                         Equipment
                     @endif
                 </h3>
-                <h3 class="text-off-white">
-                    {{$combo}}
+                <h3 class="text-gray-300">
+                    {{ $combo }}
                     @if($modMatrix['selected_index'][6]['selected'])
                         / {{$overclock['overclock_name']}}
                     @endif
                 </h3>
-            </div>
-            <div class=" w-full md:w-48 pt-4 md:mr-4">
-                <a class="button px-5" href="{{url()->previous()}}">
-                    <span class="buttonText">BACK TO KARL</span>
-                </a>
             </div>
         </div>
 
