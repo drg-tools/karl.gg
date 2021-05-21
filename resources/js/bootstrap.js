306160b82cff88db171e704d8a83949efd6a4c1e
Chase Coney <chasiepoo@gmail.com>
2021-05-21 18:28:07 -0500

    chore: remove unused js deps

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,21 +1,6 @@
 window._ = require('lodash');
 import 'alpinejs'
 
-/* todo: remove jquery and bootstrap */
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-// try {
-//     window.Popper = require('popper.js').default;
-//     window.$ = window.jQuery = require('jquery');
-//
-//     require('bootstrap');
-// } catch (e) {}
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
