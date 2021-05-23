9f7c53459bfa85d631bd8c26bf7291702d3a8618
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-05-22 22:24:57 -0400

    feat: input text styling

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -22,28 +22,12 @@
         </div>
         <div class="w-1/6 mr-3">
             <!-- For defining autocomplete -->
-            <label for="choices-multiple-remote-fetch">Options from remote source (Fetch API) &amp; limited to 5</label>
-            <select class="form-control" name="choices-multiple-remote-fetch" id="choices-multiple-remote-fetch" multiple></select>
+            <label for="choices-multiple-remote-fetch" class="block">Creator Name</label>
+            <select class="form-control text-gray-900" name="choices-multiple-remote-fetch" id="choices-multiple-remote-fetch" multiple></select>
                 
             
         
-            <label class="block">
-                <span class="text-gray-300">Creator Name</span>
-                <div class="mt-1 relative">
-                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <!-- Heroicon name: solid/mail -->
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                    </div>
-                    <input type="search" value="{{ \Request::get('search') }}" name="creator"
-                        class="focus:ring-gray-500 focus:border-gray-500 block w-full pl-10 sm:text-sm pb-3"
-                        placeholder="Search Creators">
-                </div>
-
-            </label>
+            
 
         </div>
     </div>
@@ -88,7 +72,7 @@
 
         var multipleFetch = new Choices('#choices-multiple-remote-fetch', {
           placeholder: true,
-          placeholderValue: 'Pick an Strokes record',
+          placeholderValue: 'Search Creators',
           maxItemCount: 5,
         }).setChoices(function() {
           return fetch(
