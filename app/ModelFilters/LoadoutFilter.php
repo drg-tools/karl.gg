9cd29c7a6bc142b0653cd70e2e29e3e4a435a7f0
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-05-31 19:53:58 -0400

    feat: weapon filters finally work tybg

--- app/ModelFilters/LoadoutFilter.php
+++ app/ModelFilters/LoadoutFilter.php
@@ -12,7 +12,9 @@ class LoadoutFilter extends ModelFilter
      *
      * @var array
      */
-    public $relations = [];
+    public $relations = [
+        'mods' => ['mods'], // Added so we can grab our weapon_id's
+    ];
 
     public function characters($charIds)
     {
@@ -28,6 +30,7 @@ class LoadoutFilter extends ModelFilter
 
     public function overclocks($ocVal)
     {
+        // Users requested an ability to filter out loadouts with overclocks, as they may be new to the game and don't have the $$ for the OC's yet
         if ($ocVal === null) {
             return;
         } elseif ($ocVal == 'Yes') {
@@ -39,6 +42,7 @@ class LoadoutFilter extends ModelFilter
 
     public function guide($guideVal)
     {
+        // Users requested an ability to filter out loadouts without guides, especially for new players
         if ($guideVal === null) {
             return;
         } elseif ($guideVal == 'Yes') {
@@ -50,12 +54,21 @@ class LoadoutFilter extends ModelFilter
 
     public function primaries($primaryIds)
     {
+        // I don't need to make a distinction when pulling the guns, I just need a loadout where the gun_id is this
+        // It doesn't impact UI so we don't need to know specifically "primary or secondary"
+        // These methods simply correspond with the input boxes
+        return $this->whereHas('mods', function ($query) use ($primaryIds) {
+            return $query->whereIn('gun_id', $primaryIds);
+        });
     }
 
-    // public function secondaries($secondaryIds)
-    // {
-    //     return $this->whereHas('character', function ($query) use ($secondaryIds) {
-    //         return $query->whereIn('id', $secondaryIds);
-    //     });
-    // }
+    public function secondaries($secondaryIds)
+    {
+        // I don't need to make a distinction when pulling the guns, I just need a loadout where the gun_id is this
+        // It doesn't impact UI so we don't need to know specifically "primary or secondary"
+        // These methods simply correspond with the input boxes
+        return $this->whereHas('mods', function ($query) use ($secondaryIds) {
+            return $query->whereIn('gun_id', $secondaryIds);
+        });
+    }
 }
