c3994ddf80d07bb20fd39ff77e2e3c8131b2548d
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-06-01 21:10:52 -0400

    feat: make those filters clap

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -1,82 +1,94 @@
 <div class="mb-4">
-
-    <div class="flex flex-wrap">
-        <div class="mt-1 relative rounded-md shadow-sm flex flex-col w-full md:w-1/2 mr-4">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <!-- Heroicon name: solid/mail -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-            </div>
-            <input type="search" value="{{ \Request::get('search') }}" name="search"
-                class="focus:ring-gray-500 focus:border-gray-500 block pl-10 sm:text-sm border-gray-300 text-gray-900"
-                placeholder="Search by Loadout Name">
-        </div>
-        <div class="w-1/2 flex flex-col mt-3 md:w-1/4 md:mt-1">
-            <select class="form-control" data-trigger name="characters[]" id="characters"
-                placeholder="Select Characters" multiple>
-                <option value="">Characters</option>
-                <option value="3">Driller</option>
-                <option value="1">Engineer</option>
-                <option value="4">Gunner</option>
-                <option value="2">Scout</option>
-            </select>
-        </div>
-    </div>
     <div class="flex flex-row">
-        <div class="w-1/2 md:w-2/6 mr-3 mt-4">
-            <select class="form-control" name="primaries[]" id="primaries" placeholder="Select Primary Weapons"
-                multiple>
-                <option value="">Primaries</option>
-                @foreach ($primaries as $key => $weapon)
-                    <option value="{{ $key }}">{{ $weapon }}</option>
-                @endforeach
-            </select>
-
+    <div class="mt-1 relative rounded-md shadow-sm flex flex-col w-full md:w-1/2 mr-4">
+        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <!-- Heroicon name: solid/mail -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
         </div>
-        <div class="w-1/2 md:w-2/6 mr-3 mt-4">
-            <select class="form-control" name="secondaries[]" id="secondaries" placeholder="Select Secondary Weapons"
-                multiple>
-                <option value="">Secondaries</option>
-                @foreach ($secondaries as $key => $gun)
-                    <option value="{{ $key }}">{{ $gun }}</option>
-                @endforeach
-            </select>
-
-        </div>
+        <input type="search" value="{{ \Request::get('search') }}" name="search"
+            class="focus:ring-gray-500 focus:border-gray-500 block pl-10 sm:text-sm border-gray-300 text-gray-900"
+            placeholder="Search by Loadout Name">
     </div>
-    <div class="my-2 flex sm:flex-row flex-wrap">
-        <div class="w-1/3 md:w-1/6 mr-3 mt-3">
-                <select class=" mt-1 block w-full text-gray-800" name="hasOverclock" id="hasOverclock">
-                    <option value="">Overclocks?</option>
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
-                </select>
-        </div>
-        <div class="w-1/3 md:w-1/6 mr-3 mt-3">
-                <select class="form-select mt-1 block w-full text-gray-800" name="hasGuide" id="hasGuide">
-                    <option value="">Loadout Guide?</option>
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
-                </select>
-        </div>
-        <div class="w-1/2 md:w-2/6 mr-3 mt-3">
-            <div class="">
-                <select class="form-control block" data-trigger name="overclocks[]" id="overclocks"
-                    placeholder="Select Overclocks" multiple>
-                <option value="">OC Names</option>
-                @foreach ($overclocks as $key => $oc)
-                    <option value="{{ $key }}">{{ $oc }}</option>
-                @endforeach
-            </select> 
-        </div>
-    </div>
-        <button class="ml-2 mt-3 px-6 py-1 bg-karl-orange text-gray-800 text-center font-bold sm:rounded max-h-10"
+    <button class="ml-2 mt-1 px-6 py-1 bg-karl-orange text-gray-800 text-center font-bold sm:rounded max-h-10"
             type="submit">
             Submit
         </button>
+    </div>
+    <div x-data={show:false}>
+        
+        <div x-show="show" class="mt-3">
+            <div class="flex flex-wrap">
+                
+                <div class="w-1/2 flex flex-col mt-3 md:w-1/4 md:mt-1">
+                    <select class="form-control" data-trigger name="characters[]" id="characters"
+                        placeholder="Select Characters" multiple>
+                        <option value="">Characters</option>
+                        <option value="3">Driller</option>
+                        <option value="1">Engineer</option>
+                        <option value="4">Gunner</option>
+                        <option value="2">Scout</option>
+                    </select>
+                </div>
+            </div>
+            <div class="flex flex-row">
+                <div class="w-1/2 md:w-2/6 mr-3 mt-4">
+                    <select class="form-control" name="primaries[]" id="primaries" placeholder="Select Primary Weapons"
+                        multiple>
+                        <option value="">Primaries</option>
+                        @foreach ($primaries as $key => $weapon)
+                            <option value="{{ $key }}">{{ $weapon }}</option>
+                        @endforeach
+                    </select>
+        
+                </div>
+                <div class="w-1/2 md:w-2/6 mr-3 mt-4">
+                    <select class="form-control" name="secondaries[]" id="secondaries" placeholder="Select Secondary Weapons"
+                        multiple>
+                        <option value="">Secondaries</option>
+                        @foreach ($secondaries as $key => $gun)
+                            <option value="{{ $key }}">{{ $gun }}</option>
+                        @endforeach
+                    </select>
+        
+                </div>
+            </div>
+            <div class="mt-3 flex sm:flex-row flex-wrap">
+                <div class="w-1/3 md:w-1/6 mr-3 mt-3">
+                        <select class=" mt-1 block w-full text-gray-800" name="hasOverclock" id="hasOverclock">
+                            <option value="">Overclocks?</option>
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                        </select>
+                </div>
+                <div class="w-1/3 md:w-1/6 mr-3 mt-3">
+                        <select class="form-select mt-1 block w-full text-gray-800" name="hasGuide" id="hasGuide">
+                            <option value="">Loadout Guide?</option>
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                        </select>
+                </div>
+                <div class="w-1/2 md:w-2/6 mr-3 mt-3">
+                    <div class="">
+                        <select class="form-control block" data-trigger name="overclocks[]" id="overclocks"
+                            placeholder="Select Overclocks" multiple>
+                        <option value="">OC Names</option>
+                        @foreach ($overclocks as $key => $oc)
+                            <option value="{{ $key }}">{{ $oc }}</option>
+                        @endforeach
+                    </select> 
+                </div>
+            </div>
+        </div>
+    </div>
+    <p class="flex">
+        <a x-on:click.prevent="show=!show" class="px-6 py-1 bg-gunner text-white text-center font-bold sm:rounded max-h-10 cursor-pointer focus:outline-none mr-2 mt-2">
+        Show/Hide Filters
+        </a>
+    </p> 
     </div>
 </div>
 
