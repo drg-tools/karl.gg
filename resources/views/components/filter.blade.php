2e99c646877367e223eeee72cd656021d613c95f
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-06-01 21:16:02 -0400

    feat: add characters back to main search

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -1,39 +1,38 @@
 <div class="mb-4">
-    <div class="flex flex-row">
-    <div class="mt-1 relative rounded-md shadow-sm flex flex-col w-full md:w-1/2 mr-4">
-        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <!-- Heroicon name: solid/mail -->
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
+    <div class="flex flex-wrap mt-1">
+        <div class="relative rounded-md shadow-sm flex flex-col w-full md:w-1/2 mr-4 mt-1">
+            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <!-- Heroicon name: solid/mail -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+            <input type="search" value="{{ \Request::get('search') }}" name="search"
+                class="focus:ring-gray-500 focus:border-gray-500 block pl-10 sm:text-sm border-gray-300 text-gray-900"
+                placeholder="Search by Loadout Name">
         </div>
-        <input type="search" value="{{ \Request::get('search') }}" name="search"
-            class="focus:ring-gray-500 focus:border-gray-500 block pl-10 sm:text-sm border-gray-300 text-gray-900"
-            placeholder="Search by Loadout Name">
-    </div>
-    <button class="ml-2 mt-1 px-6 py-1 bg-karl-orange text-gray-800 text-center font-bold sm:rounded max-h-10"
+        <div class="w-1/2 flex flex-col mt-3 md:w-1/4 md:mt-1">
+            <select class="form-control" data-trigger name="characters[]" id="characters"
+                placeholder="Select Characters" multiple>
+                <option value="">Characters</option>
+                <option value="3">Driller</option>
+                <option value="1">Engineer</option>
+                <option value="4">Gunner</option>
+                <option value="2">Scout</option>
+            </select>
+        </div>
+        <button class="ml-2 mt-1 px-6 py-1 bg-karl-orange text-gray-800 text-center font-bold sm:rounded max-h-10"
             type="submit">
             Submit
         </button>
     </div>
+    
     <div x-data={show:false}>
         
         <div x-show="show" class="mt-3">
-            <div class="flex flex-wrap">
-                
-                <div class="w-1/2 flex flex-col mt-3 md:w-1/4 md:mt-1">
-                    <select class="form-control" data-trigger name="characters[]" id="characters"
-                        placeholder="Select Characters" multiple>
-                        <option value="">Characters</option>
-                        <option value="3">Driller</option>
-                        <option value="1">Engineer</option>
-                        <option value="4">Gunner</option>
-                        <option value="2">Scout</option>
-                    </select>
-                </div>
-            </div>
+            
             <div class="flex flex-row">
                 <div class="w-1/2 md:w-2/6 mr-3 mt-4">
                     <select class="form-control" name="primaries[]" id="primaries" placeholder="Select Primary Weapons"
@@ -118,6 +117,7 @@
         );
         var characterSelectMultiple = new Choices(
             '#characters',
+            removeItemButton: true,
         );
         var hasOverclockSelectSingle = new Choices(
             '#hasOverclock',
