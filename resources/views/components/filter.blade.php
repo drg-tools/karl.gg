7661a4c46505a0af631952c48991a079c53ac625
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-06-05 14:09:33 -0400

    fix: submit --> search

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -86,7 +86,7 @@
             <button
                 class="inline-flex items-center text-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-500 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 w-full md:w-auto"
                 type="submit">
-                Submit
+                Search
             </button>
         </div>
     </div>
