f6934e5c28f5e17ae091a50be7c1d91e37263629
Chase Coney <chasiepoo@gmail.com>
2021-06-14 11:46:35 -0500

    fix: upgrade ratings schema

---
+++ database/migrations/2021_06_14_162848_create_ratings_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    public function up()
+    {
+        // This is a dupe of a previous published table, leaving for compat
+    }
+
+    public function down()
+    {
+
+    }
+}
