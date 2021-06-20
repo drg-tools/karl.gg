515f309fe5600e020d54bbb50d82a1980ab16721
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-06-20 18:21:06 -0400

    feat: initial class selector, persistent store

---
+++ resources/js/components/LoadoutBuilder.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="loadout-builder">
+        
+        <ClassSelect />
+    </div>
+</template>
+
+<script>
+    import ClassSelect from './ClassSelect.vue';
+
+    export default {
+        components: { ClassSelect },
+        
+
+    };
+</script>
--- resources/views/loadouts/create.blade.php
+++ resources/views/loadouts/create.blade.php
@@ -6,13 +6,8 @@
 
 @section('content')
     <div class="bg-gray-700 text-gray-300 p-4 sm:rounded">
-        <loadout-buttons></loadout-buttons>
-        <class-select></class-select>
-        <equipment-select></equipment-select>
-        <div class="flex flex-wrap">
-            <stats-display></stats-display>
-            <modification-select></modification-select>
-        </div>
+        New components will go here
+        <loadout-builder />
     </div>
 
 @endsection
