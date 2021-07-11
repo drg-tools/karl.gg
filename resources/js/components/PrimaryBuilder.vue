aef8477b58f2fd4d0cf659ba8c061e7a37aef9d1
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-07-11 13:17:05 -0400

    feat: icons to store

--- resources/js/components/PrimaryBuilder.vue
+++ resources/js/components/PrimaryBuilder.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="flex flex-row justify-between my-5">
-
+    <router-link to="/"
+    class="
+          inline-flex
+          items-center
+          text-center
+          px-4
+          py-2
+          border border-transparent
+          text-sm
+          font-medium
+          rounded-md
+          shadow-sm
+          text-white
+          bg-orange-500
+          hover:bg-orange-700
+          focus:outline-none
+          focus:ring-2 focus:ring-offset-2 focus:ring-orange-500
+          w-full
+          md:w-auto
+        "
+      >Back
+    </router-link>
     <h2>Primary Builder: -selected weapon name- / select weapon</h2>
     Weapon selector has to be here
     then mod matrix
@@ -17,9 +38,7 @@ export default {
   name: "PrimaryBuilder",
   
   methods: {
-    goBack() {
-      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
-    }
+    
   },
 };
 </script>
