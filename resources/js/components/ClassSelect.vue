95e59ca7d6919176e864ac0095f18c284ac74102
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-08-01 11:51:33 -0400

    fix: name class logical name (str)

--- resources/js/components/ClassSelect.vue
+++ resources/js/components/ClassSelect.vue
@@ -3,37 +3,37 @@
     <h1>Select class:</h1>
     <div class="grid grid-cols-2 sm:grid-cols-4 w-full mb-4">
       <div
-        v-on:click="setSelectedClass(3)"
+        v-on:click="setSelectedClass('driller')"
         class="classSelect px-4 py-2"
-        :class="[getSelected() === 3 ? 'classSelectActive' : '']"
-        v-bind:key="3"
+        :class="[getSelected() === 'driller' ? 'classSelectActive' : '']"
+        v-bind:key="'driller'"
       >
         <img src="/assets/img/50px-D_icon-hex.png" class="classIcon" />
         <h2>Driller</h2>
       </div>
       <div
-        v-on:click="setSelectedClass(1)"
+        v-on:click="setSelectedClass('engineer')"
         class="classSelect px-4 py-2"
-        :class="[getSelected() === 1 ? 'classSelectActive' : '']"
-        v-bind:key="1"
+        :class="[getSelected() === 'engineer' ? 'classSelectActive' : '']"
+        v-bind:key="'engineer'"
       >
         <img src="/assets/img/50px-E_icon-hex.png" class="classIcon" />
         <h2>Engineer</h2>
       </div>
       <div
-        v-on:click="setSelectedClass(4)"
+        v-on:click="setSelectedClass('gunner')"
         class="classSelect px-4 py-2"
-        :class="[getSelected() === 4 ? 'classSelectActive' : '']"
-        v-bind:key="4"
+        :class="[getSelected() === 'gunner' ? 'classSelectActive' : '']"
+        v-bind:key="'gunner'"
       >
         <img src="/assets/img/50px-G_icon-hex.png" class="classIcon" />
         <h2>Gunner</h2>
       </div>
       <div
-        v-on:click="setSelectedClass(2)"
+        v-on:click="setSelectedClass('scout')"
         class="classSelect px-4 py-2"
-        :class="[getSelected() === 2 ? 'classSelectActive' : '']"
-        v-bind:key="2"
+        :class="[getSelected() === 'scout' ? 'classSelectActive' : '']"
+        v-bind:key="'scout'"
       >
         <img src="/assets/img/50px-S_icon-hex.png" class="classIcon" />
         <h2>Scout</h2>
