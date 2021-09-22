b47a29ab558c57ee97fb550ab9c75ef63d0436ef
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-09-22 09:01:29 -0400

    chore: formatting

--- resources/js/components/CreditsCalculator.vue
+++ resources/js/components/CreditsCalculator.vue
@@ -1,101 +1,77 @@
 <template>
-    <div>
-        <span class="text-white text-lg block my-2">Total Costs:</span>
-        <p class="costList flex flex-wrap">
-            <span
-                class="costListItem flex items-center pr-2"
-                v-if="costObject.creditsCost > 0"
-            >
-                <img
-                    src="/assets/img/20px-Credit.png"
-                    alt="Credits"
-                    title="Credits"
-                />
-                <span>{{ costObject.creditsCost }}</span>
-            </span>
-            <span
-                class="costListItem flex items-center pr-2"
-                v-if="costObject.bismorCost > 0"
-            >
-                <img
-                    src="/assets/img/Bismor_icon.png"
-                    alt="Bismor"
-                    title="Bismor"
-                />
-                <span>{{ costObject.bismorCost }}</span>
-            </span>
-            <span
-                class="costListItem flex items-center pr-2"
-                v-if="costObject.croppaCost > 0"
-            >
-                <img
-                    src="/assets/img/Croppa_icon.png"
-                    alt="Croppa"
-                    title="Croppa"
-                />
-                <span>{{ costObject.croppaCost }}</span>
-            </span>
-            <span
-                class="costListItem flex items-center pr-2"
-                v-if="costObject.enorCost > 0"
-            >
-                <img
-                    src="/assets/img/Enor_pearl_icon.png"
-                    alt="Enor Pearl"
-                    title="Enor Pearl"
-                />
-                <span>{{ costObject.enorCost }}</span>
-            </span>
-            <span
-                class="costListItem flex items-center pr-2"
-                v-if="costObject.jadizCost > 0"
-            >
-                <img
-                    src="/assets/img/Jadiz_icon.png"
-                    alt="Jadiz"
-                    title="Jadiz"
-                />
-                <span>{{ costObject.jadizCost }}</span>
-            </span>
-            <span
-                class="costListItem flex items-center pr-2"
-                v-if="costObject.magniteCost > 0"
-            >
-                <img
-                    src="/assets/img/Magnite_icon.png"
-                    alt="Magnite"
-                    title="Magnite"
-                />
-                <span>{{ costObject.magniteCost }}</span>
-            </span>
-            <span
-                class="costListItem flex items-center pr-2"
-                v-if="costObject.umaniteCost > 0"
-            >
-                <img
-                    src="/assets/img/Umanite_icon.png"
-                    alt="Umanite"
-                    title="Umanite"
-                />
-                <span>{{ costObject.umaniteCost }}</span>
-            </span>
-        </p>
-    </div>
-    <!--  -->
+  <div>
+    <span class="text-white text-lg block my-2">Total Costs:</span>
+    <p class="costList flex flex-wrap">
+      <span
+        class="costListItem flex items-center pr-2"
+        v-if="costObject.creditsCost > 0"
+      >
+        <img src="/assets/img/20px-Credit.png" alt="Credits" title="Credits" />
+        <span>{{ costObject.creditsCost }}</span>
+      </span>
+      <span
+        class="costListItem flex items-center pr-2"
+        v-if="costObject.bismorCost > 0"
+      >
+        <img src="/assets/img/Bismor_icon.png" alt="Bismor" title="Bismor" />
+        <span>{{ costObject.bismorCost }}</span>
+      </span>
+      <span
+        class="costListItem flex items-center pr-2"
+        v-if="costObject.croppaCost > 0"
+      >
+        <img src="/assets/img/Croppa_icon.png" alt="Croppa" title="Croppa" />
+        <span>{{ costObject.croppaCost }}</span>
+      </span>
+      <span
+        class="costListItem flex items-center pr-2"
+        v-if="costObject.enorCost > 0"
+      >
+        <img
+          src="/assets/img/Enor_pearl_icon.png"
+          alt="Enor Pearl"
+          title="Enor Pearl"
+        />
+        <span>{{ costObject.enorCost }}</span>
+      </span>
+      <span
+        class="costListItem flex items-center pr-2"
+        v-if="costObject.jadizCost > 0"
+      >
+        <img src="/assets/img/Jadiz_icon.png" alt="Jadiz" title="Jadiz" />
+        <span>{{ costObject.jadizCost }}</span>
+      </span>
+      <span
+        class="costListItem flex items-center pr-2"
+        v-if="costObject.magniteCost > 0"
+      >
+        <img src="/assets/img/Magnite_icon.png" alt="Magnite" title="Magnite" />
+        <span>{{ costObject.magniteCost }}</span>
+      </span>
+      <span
+        class="costListItem flex items-center pr-2"
+        v-if="costObject.umaniteCost > 0"
+      >
+        <img src="/assets/img/Umanite_icon.png" alt="Umanite" title="Umanite" />
+        <span>{{ costObject.umaniteCost }}</span>
+      </span>
+    </p>
+  </div>
+  <!--  -->
 </template>
 
 <script>
 export default {
-    name: "CreditsCalculator",
-    data: function () {
-        return {
-            costObject: "",
-        };
-    },
-    created() {
+  name: "CreditsCalculator",
+  data: function () {
+    return {
+      costObject: "",
+    };
+  },
+  created() {
     this.unsubscribe = this.$store.subscribe((mutation, state) => {
       // TODO: make this handle equipment as well as primary and secondary
-      if (mutation.type === 'setSelectedPrimaryMod') {
+      if (mutation.type === "setSelectedPrimaryMod") {
         console.log(`Updating costs`);
         this.computeSumOfCost();
       }
@@ -104,13 +80,12 @@ export default {
   beforeDestroy() {
     this.unsubscribe();
   },
-    methods: {
-        computeSumOfCost() {
-
-            console.log("sum of cost ran");
-            console.log(this.$store.getters.getSelectedModCosts);
-            this.costObject = this.$store.getters.getSelectedModCosts;
-        },
+  methods: {
+    computeSumOfCost() {
+      console.log("sum of cost ran");
+      console.log(this.$store.getters.getSelectedModCosts);
+      this.costObject = this.$store.getters.getSelectedModCosts;
     },
+  },
 };
 </script>
