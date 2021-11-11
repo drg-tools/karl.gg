2f23867fb3e1e1e34b0606eece535a1d3701130f
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-11-11 14:12:53 -0500

    fix: updated logic for preview

--- resources/views/components/preview-top.blade.php
+++ resources/views/components/preview-top.blade.php
@@ -16,10 +16,10 @@
                 <a class="inline-flex items-center text-center px-4 py-2 mr-3 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-500 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 w-full md:w-auto"
                     href="/build/{{ $loadout->id }}">
                     @auth
-                        @if(!$loadout->creator)
-                            COPY
-                        @elseif (Auth::user()->id === $loadout->creator->id)
+                        @if (Auth::user()->id === optional($loadout->creator)->id)
                             EDIT
+                        @else
+                            COPY
                         @endif
                             
                     @endauth
