8ebbdc7c9544c5ba823502cb5d95379d055c4691
Chase Coney <chasiepoo@gmail.com>
2021-11-16 21:19:32 -0600

    refactor: extract back button component

--- resources/js/components/ClassEquipment.vue
+++ resources/js/components/ClassEquipment.vue
@@ -3,31 +3,7 @@
     <div class="w-1/2">
 
         <div class="mb-4">
-            <router-link
-                to="/"
-                class="
-                    inline-flex
-                    items-center
-                    text-center
-                    px-4
-                    py-2
-                    border border-transparent
-                    text-sm
-                    font-medium
-                    rounded-md
-                    shadow-sm
-                    text-white
-                    bg-orange-700
-                    hover:bg-orange-900
-                    focus:outline-none
-                    focus:ring-2 focus:ring-offset-2 focus:ring-orange-500
-                    w-full
-                    md:w-auto
-                    h-12
-                "
-            >
-                <i class="fas fa-chevron-left"></i>
-            </router-link>
+            <BackButton />
         </div>
 
         <div class="flex gap-4 mb-4">
@@ -59,6 +35,7 @@ import OverclockSelect from "./OverclockSelect";
 import CreditsCalculator from "./CreditsCalculator";
 
 import {mapGetters} from 'vuex'
+import BackButton from "./BackButton";
 
 
 export default {
@@ -68,6 +45,7 @@ export default {
         ModMatrix,
         OverclockSelect,
         CreditsCalculator,
+        BackButton
     },
     computed: {
         ...mapGetters([
