06703b5dfbe5438c8517708883ad1816bd39212d
Chase Coney <chasiepoo@gmail.com>
2021-11-19 17:06:41 -0600

    chore: pin packages properly

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,10 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
 // Updated, per this article: https://nono.ma/window-vue-use-is-not-a-function
-window.Vue = require('vue').default;
+window.Vue = require("vue").default;
 
 /**
  * The following block of code may be used to automatically register your
@@ -18,23 +18,23 @@ window.Vue = require('vue').default;
  */
 
 Vue.component(
-    'passport-clients',
-    require('./components/passport/Clients.vue').default
+    "passport-clients",
+    require("./components/passport/Clients.vue").default
 );
 
 Vue.component(
-    'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue').default
+    "passport-authorized-clients",
+    require("./components/passport/AuthorizedClients.vue").default
 );
 
 Vue.component(
-    'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue').default
+    "passport-personal-access-tokens",
+    require("./components/passport/PersonalAccessTokens.vue").default
 );
 
-const files = require.context('./', true, /\.vue$/i);
-files.keys().map(key => {
-    let keyId = key.split('/').pop().split('.')[0];
+const files = require.context("./", true, /\.vue$/i);
+files.keys().map((key) => {
+    let keyId = key.split("/").pop().split(".")[0];
     Vue.component(keyId, files(key).default);
 });
 
@@ -44,17 +44,17 @@ files.keys().map(key => {
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-import Vue from 'vue';
-import store from './store';
-import { Apollo } from './apollo';
-import Toasted from 'vue-toasted';
-import VPopover from 'vue-js-popover';
-import VTooltip from 'v-tooltip';
-import VModal from 'vue-js-modal';
-import VueApollo from 'vue-apollo';
-import VueClipboard from 'vue-clipboard2';
-import Vuelidate from 'vuelidate';
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import store from "./store";
+import { Apollo } from "./apollo";
+import Toasted from "vue-toasted";
+import VPopover from "vue-js-popover";
+import VTooltip from "v-tooltip";
+import VModal from "vue-js-modal";
+import VueApollo from "vue-apollo";
+import VueClipboard from "vue-clipboard2";
+import Vuelidate from "vuelidate";
+import VueRouter from "vue-router";
 
 Vue.use(VueClipboard);
 Vue.config.productionTip = false;
@@ -68,7 +68,7 @@ Vue.use(VueRouter);
 // credit to https://stackoverflow.com/questions/35070271/vue-js-components-how-to-truncate-the-text-in-the-slot-element-in-a-component
 // for this filter
 /** Vue Filters Start */
-Vue.filter('truncate', function (text, length, suffix) {
+Vue.filter("truncate", function (text, length, suffix) {
     if (text.length > length) {
         return text.substring(0, length) + suffix;
     } else {
@@ -79,12 +79,12 @@ Vue.filter('truncate', function (text, length, suffix) {
 
 // 1. Define route components.
 // These can be imported from other files
-import LoadoutBuilder from './components/LoadoutBuilder.vue'
-import LoadoutContainer from './components/LoadoutContainer.vue'
-import SelectContainer from './components/SelectContainer.vue'
-import PrimaryBuilder from './components/PrimaryBuilder.vue'
-import SecondaryBuilder from './components/SecondaryBuilder.vue'
-import EquipmentBuilder from './components/EquipmentBuilder.vue'
+import LoadoutBuilder from "./components/LoadoutBuilder.vue";
+import LoadoutContainer from "./components/LoadoutContainer.vue";
+import SelectContainer from "./components/SelectContainer.vue";
+import PrimaryBuilder from "./components/PrimaryBuilder.vue";
+import SecondaryBuilder from "./components/SecondaryBuilder.vue";
+import EquipmentBuilder from "./components/EquipmentBuilder.vue";
 
 // 2. Define some routes
 // Each route should map to a component. The "component" can
@@ -93,38 +93,37 @@ import EquipmentBuilder from './components/EquipmentBuilder.vue'
 // We'll talk about nested routes later.
 const routes = [
     {
-        path: '', component: LoadoutBuilder,
+        path: "",
+        component: LoadoutBuilder,
         children: [
             {
-                path: '', component: LoadoutContainer,
+                path: "",
+                component: LoadoutContainer,
                 children: [
-                    { path: '', component: SelectContainer },
-                    { path: '/primary-builder', component: PrimaryBuilder },
-                    { path: '/secondary-builder', component: SecondaryBuilder },
-                    { path: '/equipment-builder', component: EquipmentBuilder },
-                ]
+                    { path: "", component: SelectContainer },
+                    { path: "/primary-builder", component: PrimaryBuilder },
+                    { path: "/secondary-builder", component: SecondaryBuilder },
+                    { path: "/equipment-builder", component: EquipmentBuilder },
+                ],
             },
-
-        ]
-    }
-]
+        ],
+    },
+];
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = new VueRouter({
     routes, // short for `routes: routes`,
-})
-
-
+});
 
 const apolloProvider = new VueApollo({
-    defaultClient: Apollo
+    defaultClient: Apollo,
 });
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     store,
     router,
-    apolloProvider
+    apolloProvider,
 }).$mount("#app");
