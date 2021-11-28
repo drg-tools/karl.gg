f8fad3190aaba96d96d06211b4ddecbda2e16bc4
Chase Coney <chasiepoo@gmail.com>
2021-11-27 18:38:10 -0600

    feat(preview): add comments

--- resources/views/loadouts/preview.blade.php
+++ resources/views/loadouts/preview.blade.php
@@ -27,7 +27,33 @@
             @endif
         >
         </loadout-preview-page>
+
+        <div class="p-4">
+            <h2 class="text-karl-orange text-xl uppercase">Comments</h2>
+            <div id="disqus_thread"></div>
+        </div>
+
+
     </div>
 
 @endsection
 
+@section('scripts')
+    <script>
+        /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+         */
+        var disqus_config = function () {
+            this.page.url = "{{ url()->current() }}";  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = "{{ $loadout->id }}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://karlgg.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
+@endsection
