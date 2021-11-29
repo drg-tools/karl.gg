b29f81dd1eb2e99d54018a97b5f9419b24060acc
Chase Coney <chasiepoo@gmail.com>
2021-11-28 19:45:19 -0600

    style(comments): move to seaprate panel

--- resources/views/loadouts/preview.blade.php
+++ resources/views/loadouts/preview.blade.php
@@ -31,13 +31,12 @@
             @endif
         >
         </loadout-preview-page>
+    </div>
 
+    <div class="bg-gray-700 text-gray-300 px-3 py-2 shadow sm:rounded-md mt-4">
         <div class="p-4">
-            <h2 class="text-karl-orange text-xl uppercase">Comments</h2>
             <div id="loadout-comments"></div>
         </div>
-
-
     </div>
 
 @endsection
