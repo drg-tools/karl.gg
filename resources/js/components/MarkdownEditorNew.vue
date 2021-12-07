21ce0cdcdf2d7e118188f885e80c34d660294faa
Andrew Nolan <andrew_nolan17@yahoo.com>
2021-12-07 07:14:22 -0500

    feat: trying vue2editor

---
+++ resources/js/components/MarkdownEditorNew.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <vue-editor v-model="value" />
+    </div>
+</template>
+<script>
+import { VueEditor } from "vue2-editor";
+// https://github.com/davidroyer/vue2-editor
+// https://vuex.vuejs.org/guide/forms.html#two-way-computed-property
+export default {
+    name: "editorText",
+    components: { VueEditor },
+    data() {
+        return {
+            content: "<h1>Some initial content</h1>",
+        };
+    },
+    computed: {
+        value: {
+            get: function () {
+                return this.$store.getters.getLoadoutDescription;
+            },
+            set: function (value) {
+                this.$store.commit("setLoadoutDescription", value);
+            },
+        },
+    },
+};
+</script>
