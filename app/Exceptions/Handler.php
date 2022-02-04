2a675d3e1ca6979e1bdc515f79fcf82ebb202024
Chase Coney <chasiepoo@gmail.com>
2022-02-03 22:13:02 -0600

    fix: ignore command not found exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,7 +14,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        CommandNotFoundException::class
     ];
 
     /**
