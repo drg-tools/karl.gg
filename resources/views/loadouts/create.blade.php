9c9dca943d11dc8f02ec1d3c9cf7b5d0cd880f02
Chase Coney <chasiepoo@gmail.com>
2022-11-05 12:31:32 -0500

    chore: re-add ads

--- resources/views/loadouts/create.blade.php
+++ resources/views/loadouts/create.blade.php
@@ -6,6 +6,18 @@
 
 @section('content')
     <div class="bg-gray-700 text-gray-300 p-4 sm:rounded">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3237881937260629"
+            crossorigin="anonymous"></script>
+        <!-- Builder unit -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-3237881937260629"
+            data-ad-slot="3873388796"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         <loadout-builder />
     </div>
 
--- resources/views/loadouts/preview.blade.php
+++ resources/views/loadouts/preview.blade.php
@@ -50,6 +50,18 @@
             @endif
         >
         </loadout-preview-page>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3237881937260629"
+            crossorigin="anonymous"></script>
+        <!-- Build Preview unit -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-3237881937260629"
+            data-ad-slot="3418109138"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
     </div>
 
     <div class="bg-gray-700 text-gray-300 px-3 py-2 shadow sm:rounded-md mt-4">
