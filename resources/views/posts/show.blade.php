50c96afc11c23e4b8b5c9e3fc2fe91f2050dfdee
Chase Coney <chasiepoo@gmail.com>
2022-11-05 13:39:38 -0500

    fix: upgrade tailwind to latest

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,6 @@
     <div class="text-sm underline text-gray-300 my-2 ml-2 sm:ml-0"><a href="{{ route('blog.index') }}">Back</a></div>
     <div class="p-4 text-gray-300 bg-gray-700 sm:rounded">
         <div class="mb-4 text-gray-500">{{ $post->published_at->diffForHumans() }}</div>
-        <article class="prose lg:prose-xl max-w-full">{!! $post->content !!}</article>
+        <article class="prose-invert lg:prose-lg max-w-full">{!! $post->content !!}</article>
     </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,94 +1,93 @@
-const colors = require('tailwindcss/colors')
-
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-    purge: {
-        content: [
-            './resources/views/**/*.blade.php',
-            './resources/scss/**/*.scss',
-            './resources/js/**/*.js',
-            './resources/js/**/*.vue',
-        ],
-
-        // These options are passed through directly to PurgeCSS
-        options: {
-            safelist: [/-driller/, /-gunner/, /-engineer/, /-scout/],
-        },
-    },
+    content: [
+        "./resources/views/**/*.blade.php",
+        "./resources/scss/**/*.scss",
+        "./resources/js/**/*.js",
+        "./resources/js/**/*.vue",
+    ],
+    safelist: [
+        { pattern: /-driller/ },
+        { pattern: /-gunner/ },
+        { pattern: /-engineer/ },
+        { pattern: /-scout/ },
+    ],
     theme: {
         extend: {
             typography: (theme) => ({
                 DEFAULT: {
                     css: {
-                        color: theme('colors.gray.300'),
+                        color: theme("colors.gray.300"),
                         strong: {
-                            color: theme('colors.white'),
+                            color: theme("colors.white"),
                         },
                         '[class~="lead"]': {
-                            color: theme('colors.gray.300'),
+                            color: theme("colors.gray.300"),
                         },
                         a: {
-                            color: theme('colors.orange.500'),
+                            color: theme("colors.orange.500"),
                         },
                         blockquote: {
-                            color: theme('colors.gray.400'),
-                            borderLeftColor: theme('colors.gray.600'),
+                            color: theme("colors.gray.400"),
+                            borderLeftColor: theme("colors.gray.600"),
                         },
                         h1: {
-                            color: theme('colors.white'),
+                            color: theme("colors.white"),
                         },
                         h2: {
-                            color: theme('colors.white'),
+                            color: theme("colors.white"),
                         },
                         h3: {
-                            color: theme('colors.white'),
+                            color: theme("colors.white"),
                         },
                         h4: {
-                            color: theme('colors.white'),
+                            color: theme("colors.white"),
                         },
                         code: {
-                            color: theme('colors.gray.300'),
+                            color: theme("colors.gray.300"),
                         },
-                        'a code': {
-                            color: theme('colors.gray.300'),
+                        "a code": {
+                            color: theme("colors.gray.300"),
                         },
                         thead: {
-                            color: theme('colors.gray.300'),
-                        }
+                            color: theme("colors.gray.300"),
+                        },
                     },
                 },
             }),
             colors: {
-                orange: colors.orange,
-                'karl-orange': {
-                    DEFAULT: '#fc9e00'
+                green: colors.emerald,
+                yellow: colors.amber,
+                purple: colors.violet,
+                "karl-orange": {
+                    DEFAULT: "#fc9e00",
                 },
                 driller: {
-                    light: '#fffc31',
-                    DEFAULT: '#fdfa00',
-                    dark: '#cac800',
+                    light: "#fffc31",
+                    DEFAULT: "#fdfa00",
+                    dark: "#cac800",
                 },
                 gunner: {
-                    light: '#59a648',
-                    DEFAULT: '#407734',
-                    dark: '#335f29'
+                    light: "#59a648",
+                    DEFAULT: "#407734",
+                    dark: "#335f29",
                 },
                 engineer: {
-                    light: '#ff4131',
-                    DEFAULT: '#fd1400',
-                    dark: '#ca1000',
+                    light: "#ff4131",
+                    DEFAULT: "#fd1400",
+                    dark: "#ca1000",
                 },
                 scout: {
-                    light: '#5084fd',
-                    DEFAULT: '#2566fd',
-                    dark: '#0246e5',
-                }
-            }
-        }
+                    light: "#5084fd",
+                    DEFAULT: "#2566fd",
+                    dark: "#0246e5",
+                },
+            },
+        },
     },
-    variants: {},
     plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-    ]
-}
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+    ],
+};
